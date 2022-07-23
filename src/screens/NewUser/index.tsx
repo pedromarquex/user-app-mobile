64a7ef7513b71148ada056b86926c7a580795f8b
Pedro Marques <pedro.marques@ufpi.edu.br>
2022-07-23 15:06:44 -0300

    add new user form

--- src/screens/NewUser/index.tsx
+++ src/screens/NewUser/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
-import { Icon, Input } from "@ui-kitten/components";
+import { Icon, Spinner } from "@ui-kitten/components";
 import { useNavigation } from "@react-navigation/native";
+
+import { View } from "react-native";
+import * as S from "./styles";
+
+// {
+//   "id": "be60887a-bd2b-42ce-8c23-c0d7669b1dfc",
+//   "first_name": "User1",
+//   "last_name": "Tester",
+//   "email": "user1@mail.com",
+//   "created_at": "2022-07-23T17:40:40.502Z",
+//   "avatar": null
+// }
 
 function NewUser(): JSX.Element {
   const navigation = useNavigation();
@@ -12,14 +24,32 @@ function NewUser(): JSX.Element {
   }, [navigation]);
 
   return (
-    <Input
-      // value={value}
-      label="Primeiro Nome"
-      placeholder="Digite seu primeiro nome"
-      // caption={renderCaption}
-      accessoryLeft={<Icon name="person-outline" />}
-      // onChangeText={nextValue => setValue(nextValue)}
-    />
+    <S.FormContainer>
+      <S.TextInput
+        size="large"
+        label="Primeiro Nome"
+        placeholder="Digite seu primeiro nome"
+        accessoryLeft={<Icon name="person-outline" />}
+      />
+      <S.TextInput
+        size="large"
+        label="Último Nome"
+        placeholder="Digite seu último nome"
+        accessoryLeft={<Icon name="person-outline" />}
+      />
+      <S.TextInput
+        size="large"
+        label="Email"
+        placeholder="Digite seu melhor email"
+        accessoryLeft={<Icon name="email-outline" />}
+      />
+      <S.ConfirmButton>
+        Salvar
+        {/* <View> */}
+        {/*  <Spinner size="small" status="basic" /> */}
+        {/* </View> */}
+      </S.ConfirmButton>
+    </S.FormContainer>
   );
 }
 
---
+++ src/screens/NewUser/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+import { Input, Button } from "@ui-kitten/components";
+
+export const FormContainer = styled.View`
+  width: 90%;
+  align-self: center;
+  margin: 20px 0 0 0;
+`;
+
+export const TextInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const ConfirmButton = styled(Button)`
+  margin-top: 20px;
+`;
